6239fffb763b989e0e8ad7f5721707bd5e5c6b4f
Behdad Abedi <behdad.222@gmail.com>
2016-07-04 20:22:04 +0430

    fix

--- app/src/main/java/gardeshgary/khozestan/view/fragments/ContactUsFragment.java
+++ app/src/main/java/gardeshgary/khozestan/view/fragments/ContactUsFragment.java
@@ -1,4 +1,25 @@
 package gardeshgary.khozestan.view.fragments;
 
-public class ContactUsFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gardeshgary.khozestan.R;
+
+public class ContactUsFragment extends Fragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(
+			LayoutInflater inflater,
+			@Nullable ViewGroup container,
+			@Nullable Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+		return view;
+	}
 }
